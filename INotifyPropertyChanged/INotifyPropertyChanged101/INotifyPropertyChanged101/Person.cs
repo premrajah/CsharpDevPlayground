b85063cb8d9a0cf4f7791c53b9cf84bfa2f3d387
Prem Rajah <prajah@ctlondon.com>
2018-03-04 11:54:48 +0000

    add INotifyPropertyChanged example with WPF

---
+++ INotifyPropertyChanged/INotifyPropertyChanged101/INotifyPropertyChanged101/Person.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace INotifyPropertyChanged101
+{
+    public class Person : INotifyPropertyChanged
+    {
+        private string _firstname;
+        private string _lastname;
+        private string _fullname;
+
+        public string Firstname
+        {
+            get { return _firstname; }
+            set
+            {
+                if(_firstname != value)
+                {
+                    _firstname = value;
+                    OnPropertyChanged("Firstname");
+                    OnPropertyChanged("Fullname");
+                }
+            }
+        }
+
+        public string Lastname
+        {
+            get { return _lastname; }
+            set
+            {
+                if (_lastname != value)
+                {
+                    _lastname = value;
+                    OnPropertyChanged("Lastname");
+                    OnPropertyChanged("Fullname");
+                }
+            }
+        }
+
+        public string Fullname
+        {
+            get { return _fullname = $"{_firstname} {_lastname}"; }
+            set
+            {
+                if(_fullname != value)
+                {
+                    _fullname = value;
+                    OnPropertyChanged("Fullname");
+                }
+            }
+        }
+
+        private void OnPropertyChanged(string property)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
